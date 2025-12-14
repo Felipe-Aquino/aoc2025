1681f48f8d4cfc0bc6429ed2d6783583a38e4d96
Felipe-Aquino <felipeaqfer@gmail.com>
2025-12-14 14:56:16 -0300

    day 10 half solution

---
+++ src/day10.py
@@ -0,0 +1,261 @@
+from src import utils
+
+import math
+
+def combine(l, k):
+    if k == 0:
+        return [[]]
+
+    if len(l) < k:
+        return []
+
+    result = []
+
+    while len(l) > 0:
+        e = l.pop()
+        sub = combine(list(l), k - 1)
+        for s in sub:
+            s.insert(0, e)
+            result.append(s)
+
+    return result
+        
+def gen_ranges(values):
+    stack = [-1]
+
+    while True:
+        v = stack.pop()
+        depth = len(stack)
+
+        if v > values[depth] - 1:
+            if depth == 0:
+                break
+            else:
+                continue
+
+        else:
+            v += 1
+            stack.append(v)
+            if depth != len(values) - 1:
+                stack.append(-1)
+
+        if depth == len(values) - 1:
+            yield list(stack)
+
+class Machine:
+    def __init__(self, data: str):
+        parts = data.split()
+        light_diagram = 0
+        buttons_wiring = []
+        joltage_levels = []
+
+        v = parts[0][1:-1]
+        for i in range(len(v) - 1, -1, -1):
+            c = v[i]
+            value = 1 if c == '#' else 0
+            light_diagram = (light_diagram << 1) | value
+
+        for item in parts[1:-1]:
+            b = 0
+
+            for n in item[1:-1].split(','):
+                b = b | 1 << int(n)
+
+            buttons_wiring.append(b)
+
+        for level in parts[-1][1:-1].split(','):
+            joltage_levels.append(int(level))
+
+        self.light_diagram = light_diagram
+        self.buttons_wiring = buttons_wiring
+        self.joltage_levels = joltage_levels
+
+    def print(self):
+        print(self.light_diagram, self.buttons_wiring, self.joltage_levels)
+
+    def fewest_total_presses_for_light(self):
+        presses = 1
+
+        while presses < len(self.buttons_wiring):
+            combinations = combine(list(self.buttons_wiring), presses)
+
+            found = False
+            for c in combinations:
+                diagram = 0
+                for wiring in c:
+                    diagram = diagram ^ wiring
+
+                if diagram == self.light_diagram:
+                    found = True
+                    break
+
+            if found:
+                break
+
+            presses += 1
+
+        return presses
+
+    def fewest_total_presses_for_joltage(self):
+        data = []
+
+        unknowns_count = len(self.buttons_wiring)
+
+        max_joltage = 0
+
+        for eqn_idx, level in enumerate(self.joltage_levels):
+            cols = [0 for i in range(unknowns_count + 1)]
+
+            for j, btns in enumerate(self.buttons_wiring):
+                if (1 << eqn_idx) & btns != 0:
+                    cols[j] = 1
+
+            cols[unknowns_count] = level
+
+            max_joltage = max(max_joltage, level)
+
+            data.append(cols)
+
+        mtx = utils.Matrix(data)
+
+        # mtx.print()
+        mtx.row_echelon_reduce(False)
+        # print('after')
+        # mtx.print()
+        # print('----')
+
+        free_columns = []
+        free_columns_max_value = []
+        first_empty_row_idx = mtx.nrows
+        
+        for j in range(mtx.ncols - 1):
+            count = 0
+
+            for i in range(mtx.nrows):
+                if mtx.at(i, j) != 0:
+                    count += 1
+                    if count > 1:
+                        free_columns.append(j)
+                        break
+
+        for i in range(mtx.nrows):
+            found = -1
+
+            for j in range(mtx.ncols - 1):
+                if mtx.at(i, j) != 0:
+                    found = i
+                    break
+
+            if found == -1:
+                first_empty_row_idx = i
+                break
+
+        for col in free_columns:
+            max_value = max_joltage
+
+            for i in range(mtx.nrows):
+                coef = mtx.at(i, col)
+
+                if coef > 0:
+                    value = mtx.at(i, mtx.ncols - 1)
+
+                    v2 = math.ceil(value / coef)
+                    max_value = min(v2, max_value)
+
+                # print(i, col, max_value)
+            max_value = max(0, max_value)
+
+            # Adding 10 just to be sure
+            free_columns_max_value.append(max_value + 20)
+
+        # print(free_columns, free_columns_max_value)
+
+        min_presses = 1e9
+
+        if len(free_columns) > 0:
+            for values in gen_ranges(free_columns_max_value):
+                presses = 0
+                for v in values:
+                    presses += v
+
+                for i in range(first_empty_row_idx):
+                    v = mtx.at(i, mtx.ncols - 1)
+
+                    k = 0
+                    for col in free_columns:
+                        v -= values[k] * mtx.at(i, col)
+                        k += 1
+
+                    # print(v, math.ceil(v))
+                    if v < 0 and abs(v) > 1e-9:
+                        presses = 1e9
+                        break
+                    
+                    v2 = round(v + 1e-9)
+                    if abs(v - v2) > 0.0001:
+                        presses = 1e9
+                        break
+
+                    presses += v2
+
+                min_presses = min(min_presses, presses)
+
+                # print(values, presses)
+        else:
+            presses = 0
+
+            for i in range(first_empty_row_idx):
+                v = mtx.at(i, mtx.ncols - 1)
+                v2 = round(v)
+                presses += v2
+
+                print(v, v2);
+
+            # print('exact', presses)
+            min_presses = presses
+                
+        return min_presses
+
+    def print_stats(self):
+        eqns = len(self.joltage_levels)
+        vars = len(self.buttons_wiring)
+
+        if vars > eqns:
+            print(f'Equations = {eqns}, variables = {vars}')
+
+def part1():
+    # file = './inputs/day10-example.txt'
+    file = './inputs/day10.txt'
+
+    lines = utils.read_file_lines(file)
+
+    machines = [Machine(l) for l in lines]
+
+    total_presses = 0
+    for m in machines:
+        presses = m.fewest_total_presses_for_light()
+        total_presses += presses
+        # m.print()
+        # print(presses)
+
+    print('Fewest presses:', total_presses)
+
+def part2():
+    # file = './inputs/day10-example.txt'
+    file = './inputs/day10.txt'
+
+    lines = utils.read_file_lines(file)
+
+    machines = [Machine(l) for l in lines]
+
+    # white_list = [61, 62, 122, 129]
+    total_presses = 0
+    i = 0
+    for m in machines:
+        presses = m.fewest_total_presses_for_joltage()
+        total_presses += presses
+        i += 1
+        # print(i, presses)
+
+    print('Fewest presses:', total_presses)
+
--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,5 @@
+import array
+
 def read_file_lines(filename: str, rstrip: bool = True) -> list[str]:
     lines = []
     with open(filename) as file:
@@ -7,3 +9,96 @@ def read_file_lines(filename: str, rstrip: bool = True) -> list[str]:
             lines = [line for line in file]
 
     return lines
+
+class Matrix:
+    def __init__(self, data: list[list[int]]):
+        self.nrows = len(data)
+        self.ncols = len(data[0])
+
+        rows = []
+        for r in data:
+            rows = rows + r
+
+        self.data = array.array('d', rows)
+
+    def at(self, i, j):
+        idx = i * self.ncols + j
+        return self.data[idx]
+
+    def print(self):
+        for i in range(self.nrows):
+            row = ''
+            for j in range(self.ncols):
+                value = self.at(i, j)
+                row += str(value) + ' '
+
+            print(row)
+
+    def row_swap(self, i, j):
+        i = i * self.ncols
+        j = j * self.ncols
+
+        for _ in range(self.ncols):
+            self.data[i], self.data[j] = self.data[j], self.data[i]
+
+            i += 1
+            j += 1
+
+    def row_mult(self, i, c):
+        i = i * self.ncols
+
+        for _ in range(self.ncols):
+            self.data[i] = c * self.data[i]
+
+            i += 1
+
+    def row_mult_n_add(self, i, j, ci = 1, cj = 1):
+        i = i * self.ncols
+        j = j * self.ncols
+
+        for _ in range(self.ncols):
+            aux = ci * self.data[i] + cj * self.data[j]
+            self.data[i] = aux
+
+            i += 1
+            j += 1
+
+    def row_echelon_reduce(self, reduce_last_column = True):
+        # When solving Ax = b, b could added as an addional column
+        # to make solution finding easier
+        offset = 0 if reduce_last_column else 1
+
+        count = min(self.nrows, self.ncols - offset)
+        row_pivot_idx, col_pivot_idx = 0, 0
+
+        while row_pivot_idx < self.nrows and col_pivot_idx < (self.ncols - offset):
+            pivot_value = self.at(row_pivot_idx, col_pivot_idx)
+
+            max_pivot_value = abs(pivot_value)
+            max_pivot_idx = row_pivot_idx
+
+            for i in range(row_pivot_idx + 1, self.nrows):
+                value = abs(self.at(i, col_pivot_idx))
+
+                if max_pivot_value < value:
+                    max_pivot_value = value
+                    max_pivot_idx = i
+                    
+            if row_pivot_idx != max_pivot_idx:
+                self.row_swap(row_pivot_idx, max_pivot_idx)
+                pivot_value = self.at(row_pivot_idx, col_pivot_idx)
+
+            if pivot_value == 0 or abs(pivot_value) < 1e-9:
+                col_pivot_idx += 1
+                continue
+
+            self.row_mult(row_pivot_idx, 1 / pivot_value)
+
+            for i in range(self.nrows):
+                if i != row_pivot_idx:
+                    value = self.at(i, col_pivot_idx)
+                    self.row_mult_n_add(i, row_pivot_idx, 1, -value)
+
+            row_pivot_idx += 1
+            col_pivot_idx += 1
+
